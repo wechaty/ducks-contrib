d4a8d31d6457c0c5751785f3972d05d2ea0523fc
Huan LI (李卓桓) <zixia@zixia.net>
2020-06-03 22:31:32 +0800

    wip... prepare mocker

--- src/contrib/counter/tests.spec.ts
+++ src/contrib/counter/tests.spec.ts
@@ -1,12 +1,12 @@
 #!/usr/bin/env ts-node
 
-import { test } from 'tstest'
+import test from 'ava'
 
 import {
   validateDucksApi,
   Ducks,
 }                       from 'ducks'
-import { Wechaty }      from 'wechaty'
+import { Wechaty, Room }      from 'wechaty'
 import { PuppetMock }   from 'wechaty-puppet-mock'
 
 import {
@@ -17,10 +17,10 @@ import {
 import * as CounterApi from '.'
 
 test('validateDucksApi(counter)', async t => {
-  t.doesNotThrow(() => validateDucksApi(CounterApi), 'should pass the ducks api validating')
+  t.notThrows(() => validateDucksApi(CounterApi), 'should pass the ducks api validating')
 })
 
-test('Counter selector & operations', async (t) => {
+test('Counter selector & operations', async t => {
   const WECHATY_ID = 'wechaty-id'
 
   const ducks = new Ducks({ counter: CounterApi })
@@ -28,16 +28,18 @@ test('Counter selector & operations', async (t) => {
 
   ducks.configureStore()
 
-  t.equal(duck.selectors.getMo(), 0, 'should be 0 for mo with initialized state')
-  t.equal(duck.selectors.getMt(), 0, 'should be 0 for mt with initialized state')
+  t.teardown(() => {})
+
+  t.is(duck.selectors.getMo(), 0, 'should be 0 for mo with initialized state')
+  t.is(duck.selectors.getMt(), 0, 'should be 0 for mt with initialized state')
 
   duck.operations.mo(WECHATY_ID)
-  t.equal(duck.selectors.getMo(), 1, 'should be 1 for mo with 1 operations.mo()')
-  t.equal(duck.selectors.getMt(), 0, 'should be 0 for mt with 1 operations.mo()')
+  t.is(duck.selectors.getMo(), 1, 'should be 1 for mo with 1 operations.mo()')
+  t.is(duck.selectors.getMt(), 0, 'should be 0 for mt with 1 operations.mo()')
 
   duck.operations.mt(WECHATY_ID)
-  t.equal(duck.selectors.getMo(), 1, 'should be 1 for mo with 2 operations.mt()')
-  t.equal(duck.selectors.getMt(), 1, 'should be 1 for mt with 2 operations.mt()')
+  t.is(duck.selectors.getMo(), 1, 'should be 1 for mo with 2 operations.mt()')
+  t.is(duck.selectors.getMt(), 1, 'should be 1 for mt with 2 operations.mt()')
 })
 
 test('Counter epics', async (t) => {
@@ -52,22 +54,58 @@ test('Counter epics', async (t) => {
   function script (this: PuppetMock) {
     console.info(this.id)
   }
+
+  // const mocker = new mocker()
+
   const puppet = new PuppetMock({
-    script,
+    // mocker,
+    auto: false,
   })
 
   const bot = Wechaty.instance({ puppet })
   bot.use(WechatyRedux({ store }))
 
-  store.dispatch(WechatyApi.actions.messageEvent(bot.id, ))
+  const [ mary, mike ]  = puppet.mocker.createContacts(2)
+  const user            = puppet.mocker.createContact({
+    name: fdsfdfa,
+  })
+
+  const [ shop, yard ] = puppet.mocker.createRooms(2)
+  const group = puppet.mocker.createRoom({
+    topic: 'afs',
+    memberList: [
+      mike,
+      mary,
+      user,
+    ],
+  })
+
+  puppet.mocker.scan('fasdfasdfs')
+  puppet.mocker.login(user)
+
+  mike.say('fasfdsf').to(group)
+
+  group.add(mary).by(mike)
+  group.remove(mary).by(mike)
+  group.topic('ok').by(mike)
+  group.say('test').by(mary)
+
+
   t.equal(duck.selectors.getMo(), 0, 'should be 0 for mo with initialized state')
   t.equal(duck.selectors.getMt(), 0, 'should be 0 for mt with initialized state')
 
-  duck.operations.mo(WECHATY_ID)
-  t.equal(duck.selectors.getMo(), 1, 'should be 1 for mo with 1 operations.mo()')
-  t.equal(duck.selectors.getMt(), 0, 'should be 0 for mt with 1 operations.mo()')
+  // duck.operations.mo(WECHATY_ID)
+  user.say('fasfdsf').to(mike)
+  t.equal(duck.selectors.getMo(), 1, 'should increase to 1 for mo after user.say()')
+  t.equal(duck.selectors.getMt(), 0, 'should stay 0 for mt after user.say()')
 
-  duck.operations.mt(WECHATY_ID)
-  t.equal(duck.selectors.getMo(), 1, 'should be 1 for mo with 2 operations.mt()')
-  t.equal(duck.selectors.getMt(), 1, 'should be 1 for mt with 2 operations.mt()')
+  // duck.operations.mt(WECHATY_ID)
+  mike.say('fdafas').to(user)
+  t.equal(duck.selectors.getMo(), 1, 'should stay 1 for mo after mike.say()')
+  t.equal(duck.selectors.getMt(), 1, 'should increase to 1 for mt after mike.say()')
+
+  mary.say('fasd').to(group)
+  t.equal(duck.selectors.getMo(), 1, 'should stay 1 for mo after mike.say()')
+  t.equal(duck.selectors.getMt(), 1, 'should increase to 1 for mt after mike.say()')
+
 })
